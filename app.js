d5c79f95bd7ba268b6692431db77143b28a2c8f7
emiliantolo <emiliantolo@gmail.com>
2018-12-18 21:30:29 +0100

    ok

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express=require ("express");
 const app=express();
 const PORT = process.env.PORT || 3000;
-const bodyparser = require ("body-parser");
-
-app.use(bodyparser.json());
+let funzione=require("./function.js")
 
 app.get('/', (req, res) => res.send("Hello World"));
 
+app.get('/funzione', (req, res) =>
+	res.status(200).json({"result": funzione(req.query.a, req.query.b)}));
 app.listen(PORT);
